8889686bebabc763053a18277dcd1be4e20e2a0c
SabinoWS <sabino.th@hotmail.com>
2023-04-27 20:27:31 -0300

    Adiciona busca de linhas

--- lista.js
+++ lista.js
@@ -3,29 +3,46 @@ console.log("arquivo lista")
 import "./style/style.scss"
 import "./style/lista.scss"
 
+
+document.getElementById("busca").addEventListener("keyup", function (event) {
+    const valorBusca = event.target.value;
+    buscarNaTabela(valorBusca);
+})
+
 executar()
 
-async function executar(){
+function buscarNaTabela(valorBusca) {
+    const linhas = document.getElementById("tabela_livros").tBodies[0].rows
 
+    for (let index = 0; index < linhas.length; index++) {
+        const linhaAtual = linhas[index]
+        const textoLinha = linhaAtual.innerText
+        const contemTexto = textoLinha.includes(valorBusca)
+
+        if (contemTexto) {
+            linhaAtual.style.display = "table-row"
+        } else {
+            linhaAtual.style.display = "none"
+        }
+    }
+}
+
+async function executar() {
     // 1 - Chamar API para trazer os livros
     const url = "https://target-api-simples.cyclic.app/livros"
     const response = await fetch(url)
     const livros = await response.json()
 
     // 2 - Jogar esses livros em tela
-    const valorBusca = document.getElementById("busca").value
+    const tbody = document.getElementById("tabela_livros").tBodies[0]
 
-    const tabela = document.getElementById("tabela_livros")
-
-    const body = tabela.tBodies[0]
-
-    livros.forEach(livro => {
-        const novaLinha = tabela.tBodies[0].insertRow(0)
-        const tdId = novaLinha.insertCell(0)
-        const tdTitle = novaLinha.insertCell(1)
-        const tdDescription = novaLinha.insertCell(2)
-        tdId.innerHTML = livro.id
-        tdTitle.innerHTML = livro.title
-        tdDescription.innerHTML = livro.description
+    livros.forEach(livroAtual => {
+        const novaLinha = document.createElement('tr');
+        novaLinha.innerHTML = `
+            <td>${livroAtual.id}</td>
+            <td>${livroAtual.title}</td>
+            <td>${livroAtual.description}</td>
+        `;
+        tbody.appendChild(novaLinha);
     });
 }
